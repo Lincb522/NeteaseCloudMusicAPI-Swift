8660958abe447113c73eb4b6ce4015f737e5ac38
Lincb522 <49976696+Lincb522@users.noreply.github.com>
2026-02-07 20:55:09 +0800

    fix: 示例应用最低部署目标改为 iOS 17，恢复 ContentUnavailableView

--- Example/Package.swift
+++ Example/Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "NCMDemo",
-    platforms: [.iOS(.v16), .macOS(.v13)],
+    platforms: [.iOS(.v17), .macOS(.v14)],
     dependencies: [
         .package(name: "NeteaseCloudMusicAPI", path: ".."),
     ],
